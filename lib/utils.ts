8f4e9f00d4889afaa157cbe9231d03aa4551bab0
Pablo Silva <pablosilva@192.168.1.9>
2025-08-18 23:18:45 -0300

    test analitycs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { track } from '@vercel/analytics';
+import { track } from '@vercel/analytics/server';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
